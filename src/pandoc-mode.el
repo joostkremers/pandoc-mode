21b083c14a9c647e973c2bef8575e9ab11586606
Joost Kremers <joostkremers@users.sourceforge.net>
2010-01-30 15:25:02 +0100

    Changed year in copyright
    Added "2010".
    
    modified:   src/pandoc-mode.el

--- src/pandoc-mode.el
+++ src/pandoc-mode.el
@@ -1,6 +1,6 @@
 ;; pandoc-mode.el
 ;;
-;; Copyright (c) 2009 Joost Kremers
+;; Copyright (c) 2009-2010 Joost Kremers
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
