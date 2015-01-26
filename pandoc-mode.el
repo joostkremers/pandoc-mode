d39a07e9055a29a3c9a720e662e9ba6de6630bb5
Joost Kremers <joostkremers@fastmail.fm>
2015-01-26 13:58:34 +0100

    Update manual; increase version number.

--- pandoc-mode.el
+++ pandoc-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 31 Oct 2009
-;; Version: 2.9
+;; Version: 2.10
 ;; Keywords: text, pandoc
 
 ;; Redistribution and use in source and binary forms, with or without
