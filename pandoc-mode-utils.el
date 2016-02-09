33bf2aef4ebbb751453bea74d23ad6fc494884d1
Joost Kremers <joostkremers@fastmail.fm>
2016-02-09 01:00:27 +0100

    Fix width of verbose option

--- pandoc-mode-utils.el
+++ pandoc-mode-utils.el
@@ -999,7 +999,7 @@ evaluated."
 
 
 ;;; General writer options
-(define-pandoc-switch        verbose             (writer "V" "%-21s")        "Verbose output") ; Pandoc's README places this in the general options
+(define-pandoc-switch        verbose             (writer "V" "%-19s")        "Verbose output") ; Pandoc's README places this in the general options
 (define-pandoc-file-option   include-after-body  (writer "A" "%-19s")        "Include After Body"  'full-path)
 (define-pandoc-file-option   include-before-body (writer "B" "%-19s")        "Include Before Body" 'full-path)
 (define-pandoc-file-option   include-in-header   (writer "H" "%-19s")        "Include Header"      'full-path)
