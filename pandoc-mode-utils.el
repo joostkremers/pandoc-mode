24107eeb8ae9d256bd5edd5006da1991ab14d75c
Joost Kremers <joostkremers@fastmail.fm>
2016-02-22 13:10:26 +0100

    Update doc string of `pandoc-async-success-hook'

--- pandoc-mode-utils.el
+++ pandoc-mode-utils.el
@@ -71,7 +71,7 @@ in a synchronous subprocess."
 
 (defcustom pandoc-async-success-hook nil
   "List of functions to call when `pandoc' returns successfully.
-Note, this hook is only run when `pandoc-use-async' is set to t."
+This hook is only run when `pandoc-use-async' is set to t."
   :group 'pandoc
   :type 'hook)
 
