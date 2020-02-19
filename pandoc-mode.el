7e417a670cff080650f2c1153dd9c50fc4ac8a48
Joost Kremers <joostkremers@fastmail.fm>
2020-02-19 23:43:19 +0100

    Update version number.

--- pandoc-mode.el
+++ pandoc-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 31 Oct 2009
-;; Version: 2.28
+;; Version: 2.29
 ;; Keywords: text, pandoc
 ;; Package-Requires: ((hydra "0.10.0") (dash "2.10.0"))
 
