8b6e97734a2529ddf0eb6ed5a80f42d5b8a3a3e7
Joost Kremers <joostkremers@fastmail.com>
2025-11-07 11:10:39 +0100

    Display "none" instead of "nil" when no output file is not specified.

--- pandoc-mode.el
+++ pandoc-mode.el
@@ -952,7 +952,7 @@ it.  The arguments FORMAT-STRING and ARGS function as with
 (defun pandoc--pp-option (option)
   "Return a pretty-printed representation of OPTION."
   (if (eq option 'output)
-      (pandoc--compose-output-file-name nil nil 'expand)
+      (or (pandoc--compose-output-file-name nil nil 'expand) (propertize "none" 'face 'font-lock-comment-face))
     (or (pandoc--get option) "")))
 
 ;; Getter and setter functions
